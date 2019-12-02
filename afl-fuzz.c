9830aa99820adb4b950dbf3ce519da62ade45232
Jiahao Cai <caterpillarous@gmail.com>
2019-12-02 01:47:01 -0500

    fix FPE

--- afl-fuzz.c
+++ afl-fuzz.c
@@ -894,7 +894,7 @@ static int contains_id(int branch_id, int* branch_ids){
 
 static double calc_concurrence_score(int total_hits, int reward, int action_num) {
   int N = 10;
-  return reward / action_num + N * sqrt(log(action_num_total) / action_num);
+  return reward / action_num + N * sqrt(log(action_num_total + 1) / (action_num + 1));
 }
 
 /* you'll have to free the return pointer. */
